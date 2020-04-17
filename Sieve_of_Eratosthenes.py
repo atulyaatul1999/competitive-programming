c38f8e64431bc9e6bb4f31e3acac0e5a2698a5a6
Atul singh <59124269+atulyaatul1999@users.noreply.github.com>
2020-04-17 17:09:58 +0530

    Update and rename sieve_of_prime_number.py to Sieve_of_Eratosthenes.py

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -5,9 +5,10 @@ print(a)
 a[0]=0
 a[1]=0
 for i in range(2,int(n**.5)+1):
-    for j in range(i+1,n):
-        if j%i==0:
-            a[j]=0
+    if a[i]==1:
+        for j in range(i+1,n):
+            if j%i==0:
+                a[j]=0
 print(a)
 for i in range(n):
     if a[i]==1:
